9571afb54530f9398167bb5e1573100db7df644b
Fateehs <benzer1010@gmail.com>
2022-09-06 20:56:47 +0300

    06.09.2022

--- DataAccess/Abstract/IProductDal.cs
+++ DataAccess/Abstract/IProductDal.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿using Core.DataAccess;
+using Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Entities.Concrete;
 
 namespace DataAccess.Abstract
 {
